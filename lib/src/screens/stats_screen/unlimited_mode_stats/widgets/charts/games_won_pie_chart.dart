a18b0a2f82afba4b1d908498cd284027d6e2d1e1
MarekRudzki <102899533+MarekRudzki@users.noreply.github.com>
2025-08-24 12:56:08 +0200

    Update app to sdkVersion 35

--- lib/src/screens/stats_screen/unlimited_mode_stats/widgets/charts/games_won_pie_chart.dart
+++ lib/src/screens/stats_screen/unlimited_mode_stats/widgets/charts/games_won_pie_chart.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
-import 'package:charts_flutter_new/flutter.dart';
-
 import '/src/services/providers/stats_provider.dart';
+
+import 'package:charts_flutter_new/flutter.dart' as charts;
 
 class WinLosePieChart extends StatefulWidget {
   const WinLosePieChart({
@@ -29,70 +29,117 @@ class _WinLosePieChartState extends State<WinLosePieChart> {
       _WinLoseModel(
         status: 'Wygrane',
         counter: gamesWon,
-        color: const Color(r: 76, g: 175, b: 80),
-        style: const TextStyleSpec(
+        color: charts.ColorUtil.fromDartColor(const Color.fromARGB(255, 76, 175, 80)),
+        style: const charts.TextStyleSpec(
           fontSize: 18,
-          color: Color.white,
+          color: charts.Color.white,
         ),
       ),
       _WinLoseModel(
         status: 'Przegrane',
         counter: allGames - gamesWon,
-        color: const Color(r: 244, g: 67, b: 54),
-        style: const TextStyleSpec(
+        color: charts.ColorUtil.fromDartColor(const Color.fromARGB(255, 244, 67, 54)),
+        style: const charts.TextStyleSpec(
           fontSize: 18,
-          color: Color.white,
+          color: charts.Color.white,
         ),
       ),
     ];
 
-    return SizedBox(
-      height: 180,
-      child: PieChart<String>(
-        [
-          Series<_WinLoseModel, String>(
-            id: 'Win-lose stats',
-            colorFn: (_WinLoseModel stats, _) => stats.color,
-            domainFn: (_WinLoseModel stats, _) => stats.status,
-            measureFn: (_WinLoseModel stats, _) => stats.counter,
-            data: _data,
-            labelAccessorFn: (_WinLoseModel stats, _) =>
-                stats.counter.toString(),
-            insideLabelStyleAccessorFn: (_WinLoseModel stats, _) => stats.style,
-          ),
-        ],
-        animate: true,
-        animationDuration: const Duration(milliseconds: 1300),
-        defaultRenderer: ArcRendererConfig(
-          arcRendererDecorators: [
-            ArcLabelDecorator(
-              leaderLineColor: const Color(r: 156, g: 39, b: 176),
-              leaderLineStyleSpec: ArcLabelLeaderLineStyleSpec(
-                color: gamesWon > (allGames - gamesWon)
-                    ? const Color(r: 244, g: 67, b: 54)
-                    : const Color(r: 76, g: 175, b: 80),
-                length: 20,
-                thickness: 3,
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SizedBox(
+          height: 180,
+          width: 180,
+          child: charts.PieChart<String>(
+            [
+              charts.Series<_WinLoseModel, String>(
+                id: 'Win-lose stats',
+                colorFn: (_WinLoseModel stats, _) => stats.color,
+                domainFn: (_WinLoseModel stats, _) => stats.status,
+                measureFn: (_WinLoseModel stats, _) => stats.counter,
+                data: _data,
+                labelAccessorFn: (_WinLoseModel stats, _) => stats.counter.toString(),
+                insideLabelStyleAccessorFn: (_WinLoseModel stats, _) => stats.style,
               ),
-              outsideLabelStyleSpec: TextStyleSpec(
-                fontSize: 15,
-                color: widget.isDark ? Color.white : Color.black,
+            ],
+            animate: true,
+            animationDuration: const Duration(milliseconds: 1300),
+            defaultRenderer: charts.ArcRendererConfig(
+              arcRendererDecorators: [
+                charts.ArcLabelDecorator(
+                  leaderLineColor: const charts.Color(r: 156, g: 39, b: 176),
+                  leaderLineStyleSpec: charts.ArcLabelLeaderLineStyleSpec(
+                    color: gamesWon > (allGames - gamesWon)
+                        ? const charts.Color(r: 244, g: 67, b: 54)
+                        : const charts.Color(r: 76, g: 175, b: 80),
+                    length: 20,
+                    thickness: 3,
+                  ),
+                  outsideLabelStyleSpec: charts.TextStyleSpec(
+                    fontSize: 15,
+                    color: widget.isDark ? charts.Color.white : charts.Color.black,
+                  ),
+                ),
+              ],
+            ),
+            behaviors: [],
+          ),
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Container(
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Color.fromRGBO(76, 175, 80, 1),
+                  ),
+                  width: 10,
+                  height: 10,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 6),
+                  child: Text(
+                    'Wygrane',
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: widget.isDark ? Colors.white : Colors.black,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 40),
+              child: Row(
+                children: [
+                  Container(
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Color.fromRGBO(244, 67, 54, 1),
+                    ),
+                    width: 10,
+                    height: 10,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 6),
+                    child: Text(
+                      'Przegrane',
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: widget.isDark ? Colors.white : Colors.black,
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
           ],
-        ),
-        behaviors: [
-          DatumLegend(
-            position: BehaviorPosition.end,
-            cellPadding: const EdgeInsets.only(right: 45, top: 35),
-            outsideJustification: OutsideJustification.middleDrawArea,
-            entryTextStyle: TextStyleSpec(
-              fontSize: 13,
-              color: widget.isDark ? Color.white : Color.black,
-            ),
-          ),
-        ],
-      ),
+        )
+      ],
     );
   }
 }
@@ -100,10 +147,10 @@ class _WinLosePieChartState extends State<WinLosePieChart> {
 class _WinLoseModel {
   final String status;
   final int counter;
-  final Color color;
-  final TextStyleSpec style;
+  final charts.Color color;
+  final charts.TextStyleSpec style;
 
-  const _WinLoseModel({
+  _WinLoseModel({
     required this.status,
     required this.counter,
     required this.color,
